923e7318a2a1bbb9e0362ca5296e7eff7697cb8f
Messias R Batista <mrafaelbatista@gmail.com>
2024-03-04 18:04:53 -0300

    Criação de classe para representar pipeline

--- scripts/fusao_mercado_fev.py
+++ scripts/fusao_mercado_fev.py
@@ -1,38 +1,39 @@
 import json
 import csv
 
+from processamento_dados import Dados
 
-def leitura_json(path_json):
-    dados_json = []
-    with open(path_json, 'r') as file:
-        dados_json = json.load(file)
+# def leitura_json(path_json):
+#     dados_json = []
+#     with open(path_json, 'r') as file:
+#         dados_json = json.load(file)
     
-    return dados_json
+#     return dados_json
 
 
-def leitura_csv(path_csv):
-    dados_csv = []
-    with open(path_csv, 'r') as file:
-        spamreader = csv.DictReader(file, delimiter=',')
-        for row in spamreader:
-            dados_csv.append(row)
+# def leitura_csv(path_csv):
+#     dados_csv = []
+#     with open(path_csv, 'r') as file:
+#         spamreader = csv.DictReader(file, delimiter=',')
+#         for row in spamreader:
+#             dados_csv.append(row)
             
-    return dados_csv
+#     return dados_csv
 
 
-def leitura_dados(path, tipo_arquivo):
-    dados = []
+# def leitura_dados(path, tipo_arquivo):
+#     dados = []
     
-    if tipo_arquivo == 'csv':
-        dados = leitura_csv(path)
-    elif tipo_arquivo == 'json':
-        dados = leitura_json(path)
+#     if tipo_arquivo == 'csv':
+#         dados = leitura_csv(path)
+#     elif tipo_arquivo == 'json':
+#         dados = leitura_json(path)
         
-    return dados
+#     return dados
 
 
-def get_columns(dados):
-    return list(dados[-1].keys())
+# def get_columns(dados):
+#     return list(dados[-1].keys())
 
 
 def rename_columns(dados, key_mapping):
@@ -78,47 +79,50 @@ def salvando_dados(dados, path):
 path_json = 'data_raw/dados_empresaA.json'
 path_csv = 'data_raw/dados_empresaB.csv'
 
-
-# Iniciando a leitura
-dados_json = leitura_dados(path_json, 'json')
-nome_colunas_json = get_columns(dados_json)
-tamanho_dados_json = size_data(dados_json)
-
-print(f"Nome colunas JSON: {nome_colunas_json}")
-print(f"Tamanho dos dados json: {tamanho_dados_json}")
-
-dados_csv = leitura_dados(path_csv, 'csv')
-nome_colunas_csv = get_columns(dados_csv)
-tamanho_dados_csv = size_data(dados_csv)
-
-print(f"Nome colunas CSV: {nome_colunas_csv}")
-print(f"Tamanho dados CSV: {tamanho_dados_csv}")
+dados_empresaA = Dados(path_json, 'json')
 
 
-# Transformação dos dados
-key_mapping = {'Nome do Item': 'Nome do Produto',
-                'Classificação do Produto': 'Categoria do Produto',
-                'Valor em Reais (R$)': 'Preço do Produto (R$)',
-                'Quantidade em Estoque': 'Quantidade em Estoque',
-                'Nome da Loja': 'Filial',
-                'Data da Venda': 'Data da Venda'}
 
-dados_csv = rename_columns(dados_csv, key_mapping)
-nome_colunas_csv = get_columns(dados_csv)
-print(f"Nome colunas CSV: {nome_colunas_csv}")
+# # Iniciando a leitura
+# dados_json = leitura_dados(path_json, 'json')
+# nome_colunas_json = get_columns(dados_json)
+# tamanho_dados_json = size_data(dados_json)
 
-dados_fusao = join(dados_json, dados_csv)
-nome_colunas_fusao = get_columns(dados_fusao)
-tamanho_dados_fusao = size_data(dados_fusao)
+# print(f"Nome colunas JSON: {nome_colunas_json}")
+# print(f"Tamanho dos dados json: {tamanho_dados_json}")
 
-print(nome_colunas_fusao)
-print(tamanho_dados_fusao)
+# dados_csv = leitura_dados(path_csv, 'csv')
+# nome_colunas_csv = get_columns(dados_csv)
+# tamanho_dados_csv = size_data(dados_csv)
 
-# Salvando dados 
+# print(f"Nome colunas CSV: {nome_colunas_csv}")
+# print(f"Tamanho dados CSV: {tamanho_dados_csv}")
 
-dados_fusao_tabela = transformando_dados_tabela(dados_fusao, nome_colunas_fusao)
 
-path_dados_combinados = 'data_processed/dados_combinados.csv'
+# # Transformação dos dados
+# key_mapping = {'Nome do Item': 'Nome do Produto',
+#                 'Classificação do Produto': 'Categoria do Produto',
+#                 'Valor em Reais (R$)': 'Preço do Produto (R$)',
+#                 'Quantidade em Estoque': 'Quantidade em Estoque',
+#                 'Nome da Loja': 'Filial',
+#                 'Data da Venda': 'Data da Venda'}
 
-salvando_dados(dados_fusao_tabela, path_dados_combinados)
-print(path_dados_combinados)
+# dados_csv = rename_columns(dados_csv, key_mapping)
+# nome_colunas_csv = get_columns(dados_csv)
+# print(f"Nome colunas CSV: {nome_colunas_csv}")
+
+# dados_fusao = join(dados_json, dados_csv)
+# nome_colunas_fusao = get_columns(dados_fusao)
+# tamanho_dados_fusao = size_data(dados_fusao)
+
+# print(nome_colunas_fusao)
+# print(tamanho_dados_fusao)
+
+# # Salvando dados 
+
+# dados_fusao_tabela = transformando_dados_tabela(dados_fusao, nome_colunas_fusao)
+
+# path_dados_combinados = 'data_processed/dados_combinados.csv'
+
+# salvando_dados(dados_fusao_tabela, path_dados_combinados)
+# print(path_dados_combinados)
---
+++ scripts/processamento_dados.py
@@ -0,0 +1,42 @@
+import json
+import csv
+
+class Dados():
+    
+    def __init__(self, path, tipo_dados):
+        self.path = path
+        self.tipo_dados = tipo_dados
+        self.dados = self.leitura_dados()
+        self.nome_colunas = self.get_columns()
+        
+    
+    def leitura_json(self):
+        dados_json = []
+        with open(self.path, 'r') as file:
+            dados_json = json.load(file)
+        
+        return dados_json
+
+
+    def leitura_csv(self):
+        dados_csv = []
+        with open(self.path, 'r') as file:
+            spamreader = csv.DictReader(file, delimiter=',')
+            for row in spamreader:
+                dados_csv.append(row)
+                
+        return dados_csv
+
+
+    def leitura_dados(self):
+        dados = []
+        
+        if self.tipo_dados == 'csv':
+            dados = self.leitura_csv()
+        elif self.tipo_dados == 'json':
+            dados = self.leitura_json()
+            
+        return dados
+    
+    def get_columns(self):
+        return list(self.dados[-1].keys())
